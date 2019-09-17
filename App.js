2e3cd5d37971885c3dff564cba653532165d5322
Jacob Ankney <crosswalkastronaut@Jacobs-MBP-2-010037227081.app.byu.edu>
2019-09-17 16:02:31 -0600

    animation slides

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import {
   ScrollView,
   View,
   Text,
+  Animated,
   Button,
   StatusBar,
 } from 'react-native';
@@ -12,37 +13,60 @@ import Geolocation from '@react-native-community/geolocation';
 import MapView from 'react-native-maps';
 import Swipeable from 'react-native-gesture-handler/Swipeable';
 
+const FadeIn = props => {
+  const [location, setLocation] = useState({});
+  const [fadeAnim] = useState(new Animated.Value(0));
+  const [position] = useState(new Animated.Value(0));
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 2000,
+    }).start();
+
+    Animated.timing(position, {
+      toValue:500, 
+      duration:5000
+    }).start()
+  }, []);
+  return <Animated.View style={{
+    ...props.style, 
+    opacity:fadeAnim,
+    position:'relative', 
+    left:position
+  }}>{props.children}</Animated.View>;
+};
 
 const App = () => {
-  const [location, setLocation] = useState({});
-  useEffect(() => {
-
-  }, []);
   return (
     <View style={styles.contain}>
-
       <View>
-
-<Button title='set location' style={{position:'absolute', top:'50%'}}onPress={()=>{
-  Geolocation.getCurrentPosition(info=>{
-    setLocation(info.coords)
-  })
-}} />
-</View>
-        <MapView
-        style={{flex:1}}
-          initialRegion={{
-            latitude: location.latitude,
-            longitude: location.longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
+        <Button
+          title="set location"
+          style={{position: 'absolute', top: '50%'}}
+          onPress={() => {
+            Geolocation.getCurrentPosition(info => {
+              setLocation(info.coords);
+            });
           }}
-          showsUserLocation={false}
-          showsCompass={true}
-          zoomControlEnabled={true}
-          showsTraffic={true}
         />
+<FadeIn>
 
+        <Text>aoweifjwaoiefjwo</Text>
+</FadeIn>
+      </View>
+      {/* <MapView
+    style={{flex:1}}
+      initialRegion={{
+        latitude: 40.2222,
+        longitude: -111.1,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+      showsUserLocation={false}
+      showsCompass={true}
+      zoomControlEnabled={true}
+      showsTraffic={true}
+    /> */}
     </View>
   );
 };
